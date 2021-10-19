24503718421fc4442b6cdbbc71a5213897397a40
stephane <rochestephane@yahoo.fr>
2021-10-19 13:51:16 +0100

    Settings fees

--- src/features/operator/ClaimMarketDepositsForm/index.tsx
+++ src/features/operator/ClaimMarketDepositsForm/index.tsx
@@ -3,16 +3,19 @@ import classNames from 'classnames';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 
+import type { Market } from '../../../api-spec/generated/js/types_pb';
 import { useClaimMarketDepositsMutation } from '../operator.api';
 
 interface IFormInputs {
-  baseAsset: string;
-  quoteAsset: string;
   hash: string;
   index: number;
 }
 
-export const ClaimMarketDepositsForm = (): JSX.Element => {
+interface ClaimMarketDepositsFormProps {
+  market?: Market.AsObject;
+}
+
+export const ClaimMarketDepositsForm = ({ market }: ClaimMarketDepositsFormProps): JSX.Element => {
   const [claimMarketDeposits, { error: claimMarketDepositsError }] = useClaimMarketDepositsMutation();
   const {
     register,
@@ -21,39 +24,16 @@ export const ClaimMarketDepositsForm = (): JSX.Element => {
   } = useForm<IFormInputs>();
 
   const onSubmit: SubmitHandler<IFormInputs> = (data) => {
+    if (!market) return;
     claimMarketDeposits({
       outpointsList: [{ hash: data.hash, index: data.index }],
-      market: { baseAsset: data.baseAsset, quoteAsset: data.quoteAsset },
+      market: market,
     });
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="form-control">
-        <label className="label">
-          <span className="label-text">Base Asset</span>
-        </label>
-        <input
-          type="text"
-          className={classNames('input input-bordered', { 'mb-7': !errors.baseAsset?.message })}
-          {...register('baseAsset', { required: 'Base asset is required' })}
-        />
-        <ErrorMessage errors={errors} name="baseAsset" as={<span className="text-sm mt-1 text-error" />} />
-
-        <label className="label">
-          <span className="label-text">Quote Asset</span>
-        </label>
-        <input
-          type="text"
-          className={classNames('input input-bordered', {
-            'mb-7': !errors.quoteAsset?.message,
-          })}
-          {...register('quoteAsset', {
-            required: 'Quote asset is required',
-          })}
-        />
-        <ErrorMessage errors={errors} name="quoteAsset" as={<span className="text-sm mt-1 text-error" />} />
-
         <label className="label">
           <span className="label-text">Outpoint Hash</span>
         </label>
@@ -83,7 +63,7 @@ export const ClaimMarketDepositsForm = (): JSX.Element => {
         <ErrorMessage errors={errors} name="quoteAsset" as={<span className="text-sm mt-1 text-error" />} />
         {claimMarketDepositsError && <span className="text-sm text-error">{claimMarketDepositsError}</span>}
       </div>
-      <button className="btn btn-secondary mt-4">Claim Market Deposits</button>
+      <button className="btn btn-secondary mt-4">Deposit</button>
     </form>
   );
 };

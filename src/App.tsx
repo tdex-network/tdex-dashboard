225f5d4c9e31251c57b1c445cd9a98e38d95125b
Stéphane Roche <rochestephane@yahoo.fr>
2021-11-02 14:11:27 +0000

    Unlock modal (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Shell from './common/Shell';
+import { UnlockModalForm } from './features/walletUnlocker/UnlockModalForm';
+import { useIsReadyQuery } from './features/walletUnlocker/walletUnlocker.api';
 import { Routes } from './routes';
 
 export const App = (): JSX.Element => {
+  const { data: isReady } = useIsReadyQuery();
+  // UnlockWallet Modal
+  const [isUnlockWalletModalVisible, setIsUnlockWalletModalVisible] = useState(false);
+  const showUnlockWalletModal = () => setIsUnlockWalletModalVisible(true);
+  const handleUnlockWalletModalCancel = () => setIsUnlockWalletModalVisible(false);
+
+  useEffect(() => {
+    if (isReady && !isReady.isUnlocked) showUnlockWalletModal();
+  }, [isReady]);
+
   return (
-    <Shell>
-      <Routes />
-    </Shell>
+    <>
+      <Shell>
+        <Routes />
+      </Shell>
+      <UnlockModalForm
+        closable={false}
+        handleUnlockWalletModalCancel={handleUnlockWalletModalCancel}
+        isUnlockWalletModalVisible={isUnlockWalletModalVisible}
+      />
+    </>
   );
 };
--- src/features/walletUnlocker/UnlockModalForm.tsx
+++ src/features/walletUnlocker/UnlockModalForm.tsx
@@ -1,7 +1,7 @@
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { Button, Form, Input, Modal, notification } from 'antd';
 import classNames from 'classnames';
-import React, { useState } from 'react';
+import React from 'react';
 
 import { useUnlockWalletMutation } from './walletUnlocker.api';
 
@@ -9,30 +9,32 @@ interface IFormInputs {
   password: string;
 }
 
-export const UnlockModalForm = (): JSX.Element => {
+interface UnlockModalFormProps {
+  closable?: boolean;
+  handleUnlockWalletModalCancel: () => void;
+  isUnlockWalletModalVisible: boolean;
+}
+
+export const UnlockModalForm = ({
+  closable = true,
+  handleUnlockWalletModalCancel,
+  isUnlockWalletModalVisible,
+}: UnlockModalFormProps): JSX.Element => {
   const [form] = Form.useForm<IFormInputs>();
   const [unlockWallet, { error: unlockWalletError, isLoading }] = useUnlockWalletMutation();
-
-  // UnlockWallet Modal
-  const [isUnlockWalletModalVisible, setIsUnlockWalletModalVisible] = useState(false);
-
-  const showUnlockWalletModal = () => {
-    setIsUnlockWalletModalVisible(true);
-  };
 
   const handleUnlockWalletModalOk = async () => {
     try {
       const values = await form.validateFields();
       const res = await unlockWallet({ password: values.password });
-      console.log('res', res);
-      form.resetFields();
       // @ts-ignore
       if (res?.error) {
         // @ts-ignore
         throw new Error(res?.error);
       } else {
-        setIsUnlockWalletModalVisible(false);
         notification.success({ message: 'Wallet unlocked successfully' });
+        form.resetFields();
+        handleUnlockWalletModalCancel();
       }
     } catch (err) {
       // @ts-ignore
@@ -40,33 +42,38 @@ export const UnlockModalForm = (): JSX.Element => {
     }
   };
 
-  const handleUnlockWalletModalCancel = () => {
-    setIsUnlockWalletModalVisible(false);
-  };
-
   return (
-    <>
-      <Button type="text" style={{ padding: 0 }} onClick={showUnlockWalletModal}>
-        Unlock Wallet
-      </Button>
-      <Modal
-        title="Unlock Wallet"
-        visible={isUnlockWalletModalVisible}
-        onOk={handleUnlockWalletModalOk}
-        onCancel={handleUnlockWalletModalCancel}
-        confirmLoading={isLoading}
-      >
-        <Form layout="vertical" form={form} name="unlock_wallet_form" wrapperCol={{ span: 16 }}>
-          <Form.Item
-            label="Password"
-            name="password"
-            className={classNames({ 'has-error': unlockWalletError })}
-          >
-            <Input.Password iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} />
-          </Form.Item>
-          {unlockWalletError && <p className="error">{unlockWalletError}</p>}
-        </Form>
-      </Modal>
-    </>
+    <Modal
+      style={{ textAlign: 'center' }}
+      title="Unlock Wallet"
+      visible={isUnlockWalletModalVisible}
+      onOk={handleUnlockWalletModalOk}
+      onCancel={handleUnlockWalletModalCancel}
+      confirmLoading={isLoading}
+      closable={closable}
+      maskClosable={closable}
+      // @ts-ignore
+      maskStyle={{ 'backdrop-filter': 'blur(6px)' }}
+      footer={
+        closable
+          ? undefined
+          : [
+              <Button key="unlock" loading={isLoading} onClick={handleUnlockWalletModalOk}>
+                Unlock
+              </Button>,
+            ]
+      }
+    >
+      <Form layout="vertical" form={form} name="unlock_wallet_form" wrapperCol={{ span: 16 }}>
+        <Form.Item
+          label="Password"
+          name="password"
+          className={classNames({ 'has-error': unlockWalletError })}
+        >
+          <Input.Password iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} />
+        </Form.Item>
+        {unlockWalletError && <p className="error">{unlockWalletError}</p>}
+      </Form>
+    </Modal>
   );
 };

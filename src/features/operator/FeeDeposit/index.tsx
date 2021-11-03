fc2bd06679bf14bb64ff69abe7b1173964817334
Stéphane Roche <rochestephane@yahoo.fr>
2021-11-03 13:31:18 +0000

    Deposit page (#25)

--- src/features/operator/FeeDeposit/index.tsx
+++ src/features/operator/FeeDeposit/index.tsx
@@ -1,7 +1,12 @@
-import { Col, Row, Typography } from 'antd';
+import './feeDeposit.less';
+import Icon from '@ant-design/icons';
+import { Breadcrumb, Button, Col, Row, Typography } from 'antd';
 import QRCode from 'qrcode.react';
 import React from 'react';
 
+import { ReactComponent as chevronRight } from '../../../assets/images/ chevron-right.svg';
+import alertOctogon from '../../../assets/images/alert-octagon.svg';
+import { HOME_ROUTE } from '../../../routes/constants';
 import { useGetFeeFragmenterAddressQuery } from '../operator.api';
 
 const { Text, Title } = Typography;
@@ -11,7 +16,13 @@ export const FeeDeposit = (): JSX.Element => {
 
   return (
     <>
-      <Row gutter={{ xs: 8, sm: 12, md: 16 }}>
+      <Breadcrumb separator={<Icon component={chevronRight} />}>
+        <Breadcrumb.Item>
+          <a href={HOME_ROUTE}>Dashboard</a>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>Deposit</Breadcrumb.Item>
+      </Breadcrumb>
+      <Row className="fee-deposit-container" gutter={{ xs: 25, sm: 30, md: 35 }}>
         <Col span={12}>
           <Title level={4}>READ CAREFULLY!</Title>
           <p>
@@ -27,16 +38,27 @@ export const FeeDeposit = (): JSX.Element => {
           </p>
         </Col>
         <Col span={12}>
-          <Row>
-            <Col span={12} offset={6}>
-              <QRCode className="w-100 h-100" level="H" value={feeFragmenterAddress || ''} />
+          <Row className="grey-panel">
+            <Col span={8} offset={8}>
+              <QRCode className="qr-code" level="H" value={feeFragmenterAddress || ''} />
             </Col>
           </Row>
-          <Row className="mt-6">
-            <Col span={20} offset={2}>
-              <Text className="white-value white-value__medium break-all" copyable>
+          <Row className="my-6">
+            <Col span={21} offset={1}>
+              <Text className="address" copyable>
                 {feeFragmenterAddress ?? 'N/A'}
               </Text>
+            </Col>
+          </Row>
+          <Row className="grey-panel text-center">
+            <Col span={20} offset={2}>
+              <img src={alertOctogon} alt="alert" className="mb-2" />
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              <Row>
+                <Col span={20} offset={2}>
+                  <Button>CONFIRM DEPOSIT</Button>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>

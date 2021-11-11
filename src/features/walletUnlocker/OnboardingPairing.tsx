fc8aebed2675aa3f1935d52027a2c6d36422dfc2
stephane <rochestephane@yahoo.fr>
2021-11-11 17:39:50 +0000

    Download and install TLS cert

--- src/features/walletUnlocker/OnboardingPairing.tsx
+++ src/features/walletUnlocker/OnboardingPairing.tsx
@@ -1,6 +1,6 @@
 import './onboardingPairing.less';
-import { Button, Col, Form, Input, notification, Row, Typography } from 'antd';
-import React from 'react';
+import { Button, Col, Form, Input, Modal, notification, Row, Typography } from 'antd';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useTypedDispatch } from '../../app/store';
@@ -15,6 +15,8 @@ interface IFormInputs {
 
 export const OnboardingPairing = (): JSX.Element => {
   const [form] = Form.useForm<IFormInputs>();
+  const [isValidCert, setIsValidCert] = useState<boolean>(false);
+  const [isDownloadCertModalVisible, setIsDownloadCertModalVisible] = useState<boolean>(false);
   const navigate = useNavigate();
   const dispatch = useTypedDispatch();
 
@@ -29,40 +31,83 @@ export const OnboardingPairing = (): JSX.Element => {
     }
   };
 
-  // function downloadBase64File(contentType: string, base64Data: string, fileName: string) {
-  //   const data = '-----BEGIN CERTIFICATE-----\n' + base64Data + '\n-----END CERTIFICATE-----';
-  //   const linkSource = `data:${contentType};base64,${data}`;
-  //   const downloadLink = document.createElement('a');
-  //   //downloadLink.style = 'none';
-  //   downloadLink.href = linkSource;
-  //   downloadLink.download = fileName;
-  //   downloadLink.click();
-  // }
+  function downloadBase64File(contentType: string, base64Data: string, fileName: string) {
+    const paramsString = base64Data.split('?')[1];
+    const params = new URLSearchParams(paramsString);
+    const { cert } = Object.fromEntries(params.entries());
+    if (!cert) {
+      notification.error({ message: 'tdexdconnecturl not valid' });
+      return;
+    }
+    setIsValidCert(true);
+    // Add line breaks
+    let certFormatted = cert.replace(/(.{64})/g, '$1\n');
+    // https://github.com/tdex-network/tdex-daemon/issues/499
+    certFormatted = certFormatted.replaceAll('-', '+');
+    certFormatted = certFormatted.replaceAll('_', '/');
+    certFormatted += '==';
+    //
+    const certWithHeaders = '-----BEGIN CERTIFICATE-----\n' + certFormatted + '\n-----END CERTIFICATE-----';
+    const uriContent = URL.createObjectURL(new Blob([certWithHeaders], { type: contentType }));
+    const link = document.createElement('a');
+    link.setAttribute('href', uriContent);
+    link.setAttribute('download', fileName);
+    const event = new MouseEvent('click');
+    link.dispatchEvent(event);
+  }
+
+  const showDownloadCertModal = () => {
+    setIsDownloadCertModalVisible(true);
+  };
 
   return (
-    <Row id="pairing" className="w-100">
-      <Col span={18} offset={3}>
-        <Title level={2} className="dm-sans dm-sans__xx dm-sans__bold text-center mb-8">
-          Welcome on TDEX Dashboard
+    <>
+      <Row id="pairing" className="w-100">
+        <Col span={18} offset={3}>
+          <Title level={2} className="dm-sans dm-sans__xx dm-sans__bold text-center mb-8">
+            Welcome on TDEX Dashboard
+          </Title>
+          <Form
+            onFinish={onFinish}
+            layout="vertical"
+            form={form}
+            name="pairing_form"
+            wrapperCol={{ span: 24 }}
+            validateTrigger="onBlur"
+          >
+            <Form.Item name="tdexdConnectUrl" className="mb-8">
+              <Input.TextArea
+                placeholder="Paste the tdexdconnect url or scan QR code"
+                onPaste={showDownloadCertModal}
+              />
+            </Form.Item>
+            <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+              <Button htmlType="submit" className="w-100" disabled={!isValidCert}>
+                PAIR DASHBOARD
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+      <Modal
+        title="Download & install TLS Certificate"
+        visible={isDownloadCertModalVisible}
+        onOk={() => {
+          downloadBase64File('application/x-pem-file', form.getFieldValue('tdexdConnectUrl'), 'cert.pem');
+          setIsDownloadCertModalVisible(false);
+        }}
+        onCancel={() => setIsDownloadCertModalVisible(false)}
+      >
+        <Title className="dm-sans dm-sans__xx dm-sans__bold" level={3}>
+          Please read carefully
         </Title>
-        <Form
-          onFinish={onFinish}
-          layout="vertical"
-          form={form}
-          name="pairing_form"
-          wrapperCol={{ span: 24 }}
-          validateTrigger="onBlur"
-        >
-          <Form.Item name="tdexdConnectUrl" className="mb-8">
-            <Input.TextArea placeholder="Paste the tdexdconnect url or scan QR code" />
-          </Form.Item>
-          <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-            <Button htmlType="submit" className="w-100">
-              PAIR DASHBOARD
-            </Button>
-          </Form.Item>
-        </Form>
-      </Col>
-    </Row>
+        <ol className="dm-mono dm-mono__x">
+          <li>Accept to download the daemon TLS certificate</li>
+          <li>Double click on it to install</li>
+          <li>Mark it as "always trust"</li>
+          <li>Click PAIR DASHBOARD button to finish onboarding</li>
+        </ol>
+      </Modal>
+    </>
   );
 };

ee88c1c2b29dddd92043b12685ed968f6fab977d
altafan <pietro@vulpem.com>
2021-11-19 12:36:10 +0100

    Revert some of the changes & handle base url/macaroon against tauri/browser mode

--- src/features/settings/settingsSlice.ts
+++ src/features/settings/settingsSlice.ts
@@ -1,5 +1,6 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
+import type { Metadata } from 'grpc-web';
 
 import { OperatorClient } from '../../api-spec/generated/js/OperatorServiceClientPb';
 import { WalletClient } from '../../api-spec/generated/js/WalletServiceClientPb';
@@ -15,21 +16,20 @@ export interface SettingsState {
   explorerLiquidAPI: string;
   explorerLiquidUI: string;
   assets: Asset[];
+  baseUrl: string;
+  useProxy: boolean;
   marketsLabelled?: MarketLabelled[];
-  tdexDaemonBaseUrl: string;
   macaroonCredentials?: string;
-  proxyUrl: string;
-  isInitialized: boolean;
   tdexdConnectUrl?: string;
 }
 
 export const initialState: SettingsState = {
-  chain: network.chain,
-  explorerLiquidAPI: network.explorerLiquidAPI,
-  explorerLiquidUI: network.explorerLiquidUI,
-  tdexDaemonBaseUrl: network.tdexDaemonBaseUrl,
+  chain: 'regtest',
+  explorerLiquidAPI: 'https://blockstream.info/liquid/api',
+  explorerLiquidUI: 'https://blockstream.info/liquid',
+  baseUrl: '__TAURI__' in window ? 'http://localhost:3030' : 'http://localhost:9000',
   assets: featuredAssets,
-  isInitialized: false,
+  useProxy: '__TAURI__' in window,
 };
 
 export const settingsSlice = createSlice({
@@ -45,8 +45,10 @@ export const settingsSlice = createSlice({
     setMarketLabelled: (state, action: PayloadAction<MarketLabelled>) => {
       state.marketsLabelled = [...(state.marketsLabelled || []), action.payload];
     },
-    setTdexDaemonBaseUrl: (state, action: PayloadAction<string>) => {
-      state.tdexDaemonBaseUrl = action.payload;
+    setBaseUrl: (state, action: PayloadAction<string>) => {
+      if (!state.useProxy) {
+        state.baseUrl = action.payload;
+      }
     },
     setMacaroonCredentials: (state, action: PayloadAction<string | undefined>) => {
       state.macaroonCredentials = action.payload;
@@ -54,11 +56,8 @@ export const settingsSlice = createSlice({
     setTdexdConnectUrl: (state, action: PayloadAction<string | undefined>) => {
       state.tdexdConnectUrl = action.payload;
     },
-    setIsInitialized: (state, action: PayloadAction<boolean>) => {
-      state.isInitialized = action.payload;
-    },
     logout: (state) => {
-      state.isInitialized = false;
+      state.macaroonCredentials = undefined;
       state.tdexdConnectUrl = undefined;
     },
     resetSettings: () => initialState,
@@ -69,12 +68,17 @@ export function selectChain(state: RootState): 'liquid' | 'regtest' {
   return state.settings.chain;
 }
 
-export function selectProxyUrl(state: RootState): string {
-  return state.settings.proxyUrl;
+export function selectBaseUrl(state: RootState): string {
+  return state.settings.baseUrl;
 }
 
-export function selectIsInitialized(state: RootState): boolean {
-  return state.settings.isInitialized;
+export function selectMacaroonCreds(state: RootState): Metadata | null {
+  if (state.settings.useProxy && state.settings.macaroonCredentials) {
+    return {
+      macaroon: state.settings.macaroonCredentials,
+    };
+  }
+  return null
 }
 
 export function selectMarketLabelled(state: RootState): MarketLabelled[] | undefined {
@@ -83,26 +87,25 @@ export function selectMarketLabelled(state: RootState): MarketLabelled[] | undef
 
 //
 export function selectOperatorClient(state: RootState): OperatorClient {
-  return new OperatorClient(selectProxyUrl(state));
+  return new OperatorClient(selectBaseUrl(state));
 }
 
 export function selectWalletClient(state: RootState): WalletClient {
-  return new WalletClient(selectProxyUrl(state));
+  return new WalletClient(selectBaseUrl(state));
 }
 
 export function selectWalletUnlockerClient(state: RootState): WalletUnlockerClient {
-  return new WalletUnlockerClient(selectProxyUrl(state));
+  return new WalletUnlockerClient(selectBaseUrl(state));
 }
 
 export const {
-  setTdexDaemonBaseUrl,
+  setBaseUrl,
   setMacaroonCredentials,
   setTdexdConnectUrl,
   resetSettings,
   logout,
   saveAsset,
   setMarketLabelled,
-  setIsInitialized
 } = settingsSlice.actions;
 
 export default settingsSlice.reducer;

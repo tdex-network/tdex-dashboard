ac1de70e8c3a9ac67f7bea1caf4e582ae76af4e3
Pietralberto Mazza <pietro@vulpem.com>
2021-11-23 16:18:04 +0100

    Rpc proxy (#69)
    * Add embedded RPC proxy
    
    * Remove macaroon and daemon host from state && Add proxy url and flag for daemon initialized
    
    * Do not include macaroon in request headers
    
    * Fix macaroon encoding
    
    * Fixes
    
    * wip
    
    * Ignore tdexd folder
    
    * Add commands to run clean dockerized tdexd
    
    * Revert changes
    
    * Revert some of the changes & handle base url/macaroon against tauri/browser mode
    
    * Prettier
    
    * Fix tdexd instance scripts
    
    * Move unlocking logic down to Home
    
    * a
    
    * Add startProxy think
    
    * Add external bin to tauri conf
    
    * Remove gh token
    
    * Source url from config.json
    
    * Revert changes to initialState & remove logs
    
    * Move to settings.json
    
    * Remove CGO
    
    * Pass connect url to proxy with REST endpoint
    
    * Add hook and thunk to start or connect to proxy
    
    * Fix showing unlock modal
    
    * Fixes
    
    * Fmt
    
    * Remove unused var
    
    * Fixes after review

---
+++ go.mod
@@ -0,0 +1,40 @@
+module github.com/tdex.network/tdex-dashboard
+
+go 1.17
+
+require (
+	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce
+	github.com/gorilla/mux v1.8.0
+	github.com/improbable-eng/grpc-web v0.13.0
+	github.com/mwitkow/grpc-proxy v0.0.0-20181017164139-0f1106ef9c76
+	github.com/sirupsen/logrus v1.8.1
+	github.com/tdex-network/tdex-daemon v0.7.1
+	github.com/tdex-network/tdex-daemon/pkg/macaroons v0.0.0-20210813140257-70d50a8b72a4
+	google.golang.org/grpc v1.42.0
+	gopkg.in/macaroon.v2 v2.1.0
+)
+
+require (
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
+	github.com/btcsuite/btcwallet v0.12.0 // indirect
+	github.com/btcsuite/btcwallet/walletdb v1.3.5 // indirect
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
+	github.com/go-macaroon-bakery/macaroonpb v1.0.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/lightningnetwork/lnd/healthcheck v1.0.0 // indirect
+	github.com/lightningnetwork/lnd/ticker v1.0.0 // indirect
+	github.com/rogpeppe/fastuuid v1.2.0 // indirect
+	github.com/rs/cors v1.7.0 // indirect
+	github.com/tdex-network/tdex-daemon/pkg/securestore v0.0.0-20210813140257-70d50a8b72a4 // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210908233432-aa78b53d3365 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/errgo.v1 v1.0.1 // indirect
+	gopkg.in/macaroon-bakery.v2 v2.3.0 // indirect
+)
---
+++ rpc_proxy.go
@@ -0,0 +1,284 @@
+package main
+
+import (
+	"context"
+	"crypto/x509"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	grpcProxy "github.com/mwitkow/grpc-proxy/proxy"
+	log "github.com/sirupsen/logrus"
+	"github.com/tdex-network/tdex-daemon/pkg/macaroons"
+	"github.com/tdex-network/tdex-daemon/pkg/tdexdconnect"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/backoff"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"gopkg.in/macaroon.v2"
+)
+
+const (
+	defaultConnectTimeout = 15 * time.Second
+	defaultServerTimeout  = 10 * time.Second
+	defaultServerAddr     = ":3030"
+)
+
+var (
+	// maxMsgRecvSize is the largest message our proxy will receive. We
+	// set this to 200MiB atm.
+	maxMsgRecvSize = grpc.MaxCallRecvMsgSize(1 * 1024 * 1024 * 200)
+
+	// flag --addr to customize host:port on which the HTTP server should listen.
+	httpServerAddr = flag.String(
+		"insecure-httplisten",
+		defaultServerAddr,
+		"the host:port address which the HTTP proxy should listen on",
+	)
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	proxy, err := newRpcProxy()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer proxy.Stop()
+
+	chErr := make(chan error, 1)
+	go func() {
+		if err := proxy.Start(*httpServerAddr); err != nil {
+			chErr <- err
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+	select {
+	case <-sigChan:
+		log.Info("shutting down")
+		return
+	case <-chErr:
+		log.Fatal(err)
+	}
+}
+
+type rpcProxy struct {
+	tdexdConn    *grpc.ClientConn
+	grpcServer   *grpc.Server
+	grpcWebProxy *grpcweb.WrappedGrpcServer
+	httpServer   *http.Server
+
+	lock *sync.Mutex
+}
+
+func newRpcProxy() (*rpcProxy, error) {
+	proxy := &rpcProxy{
+		lock: &sync.Mutex{},
+	}
+	proxy.newGRPCWebProxy()
+	proxy.newHTTPServer()
+	return proxy, nil
+}
+
+func (p *rpcProxy) Start(addr string) error {
+	httpListener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s: %s", addr, err)
+	}
+	log.Infof("start listening on %s", addr)
+	return p.httpServer.Serve(httpListener)
+}
+
+func (p *rpcProxy) Stop() error {
+	log.Debug("stopping http server")
+	if err := p.httpServer.Close(); err != nil {
+		return err
+	}
+
+	log.Debug("stopping internal gRPC server")
+	p.grpcServer.Stop()
+
+	if p.tdexdConn != nil {
+		log.Debug("closing connection with TDEX daemon")
+		return p.tdexdConn.Close()
+	}
+	return nil
+}
+
+func (p *rpcProxy) newGRPCWebProxy() {
+	grpcServer := grpc.NewServer(
+		grpc.CustomCodec(grpcProxy.Codec()),
+		grpc.UnknownServiceHandler(
+			grpcProxy.TransparentHandler(p.director),
+		),
+	)
+
+	opts := []grpcweb.Option{
+		grpcweb.WithWebsockets(true),
+		grpcweb.WithWebsocketPingInterval(2 * time.Minute),
+		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
+	}
+
+	grpcWebProxy := grpcweb.WrapServer(grpcServer, opts...)
+	p.grpcServer, p.grpcWebProxy = grpcServer, grpcWebProxy
+}
+
+func (p *rpcProxy) newHTTPServer() {
+	httpHandler := p.newHTTPHandler()
+	httpServer := &http.Server{
+		// To make sure that long-running calls and indefinitely opened
+		// streaming connections aren't terminated by the internal
+		// proxy, we need to disable all timeouts except the one for
+		// reading the HTTP headers. That timeout shouldn't be removed
+		// as we would otherwise be prone to the slowloris attack where
+		// an attacker takes too long to send the headers and uses up
+		// connections that way.
+		WriteTimeout:      0,
+		IdleTimeout:       0,
+		ReadTimeout:       0,
+		ReadHeaderTimeout: defaultServerTimeout,
+		Handler:           httpHandler,
+	}
+	p.httpServer = httpServer
+}
+
+func (p *rpcProxy) director(
+	ctx context.Context, requireURI string,
+) (context.Context, *grpc.ClientConn, error) {
+	// If this header is present in the request from the web client,
+	// the actual connection to the backend will not be established.
+	// https://github.com/improbable-eng/grpc-web/issues/568
+	md, _ := metadata.FromIncomingContext(ctx)
+	mdCopy := md.Copy()
+	delete(mdCopy, "connection")
+
+	outCtx := metadata.NewOutgoingContext(ctx, mdCopy)
+
+	return outCtx, p.tdexdConn, nil
+}
+
+func (p *rpcProxy) newHTTPHandler() *mux.Router {
+	router := mux.NewRouter()
+
+	// Forward all requests to taget TDEX daemon.
+	router.HandleFunc("/connect", p.handleConnectRequest).Methods(http.MethodPost, http.MethodOptions)
+	router.PathPrefix("/").HandlerFunc(p.forwardGRPCRequest)
+
+	return router
+}
+
+func (p *rpcProxy) forwardGRPCRequest(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Access-Control-Allow-Origin", "*")
+	resp.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	resp.Header().Set("Access-Control-Allow-Headers", "*")
+
+	if p.grpcWebProxy.IsGrpcWebRequest(req) ||
+		p.grpcWebProxy.IsGrpcWebSocketRequest(req) ||
+		p.grpcWebProxy.IsAcceptableGrpcCorsRequest(req) {
+		if req.Method != "OPTIONS" {
+			log.Infof("handling gRPC web request: %s", req.URL.Path)
+		}
+		p.grpcWebProxy.ServeHTTP(resp, req)
+		return
+	}
+	if req.Method != "OPTIONS" {
+		log.Infof("handling gRPC request: %s", req.URL.Path)
+	}
+	p.grpcServer.ServeHTTP(resp, req)
+}
+
+func (p *rpcProxy) handleConnectRequest(resp http.ResponseWriter, req *http.Request) {
+	log.Info("handling connect request: %s", req.URL.Path)
+	body := make(map[string]interface{})
+	if err := json.NewDecoder(req.Body).Decode(&body); err != nil {
+		errMsg := fmt.Sprintf("invalid request body: %v", err)
+		http.Error(resp, errMsg, http.StatusBadRequest)
+		return
+	}
+
+	connectUrl, ok := body["url"].(string)
+	if !ok || len(connectUrl) <= 0 {
+		http.Error(resp, "url is undefined", http.StatusBadRequest)
+		return
+	}
+
+	addr, tlsCert, macaroon, err := tdexdconnect.Decode(connectUrl)
+	if err != nil {
+		errMsg := fmt.Sprintf("failed to parse url: %s", err)
+		http.Error(resp, errMsg, http.StatusBadRequest)
+		return
+	}
+
+	tdexdConn, err := createGRPCConn(addr, macaroon, tlsCert)
+	if err != nil {
+		errMsg := fmt.Sprintf("failed to connect to daemon: %s", err)
+		http.Error(resp, errMsg, http.StatusInternalServerError)
+		return
+	}
+
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.tdexdConn = tdexdConn
+
+	json.NewEncoder(resp).Encode(map[string]interface{}{
+		"status": "connected",
+	})
+}
+
+func createGRPCConn(
+	daemonEndpoint string, macBytes, certBytes []byte,
+) (*grpc.ClientConn, error) {
+	var opts []grpc.DialOption
+
+	// TLS credentials
+	cert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse TLS certificate: %s", err)
+	}
+	cp := x509.NewCertPool()
+	cp.AddCert(cert)
+	tlsCreds := credentials.NewClientTLSFromCert(cp, "")
+
+	if len(macBytes) > 0 {
+		// macaroons credentials
+		mac := &macaroon.Macaroon{}
+		if err := mac.UnmarshalBinary(macBytes); err != nil {
+			return nil, fmt.Errorf("could failed to parse macaroon: %s", err)
+		}
+		macCreds := macaroons.NewMacaroonCredential(mac, true)
+		opts = append(opts, grpc.WithPerRPCCredentials(macCreds))
+	}
+
+	opts = append(
+		opts,
+		grpc.WithCodec(grpcProxy.Codec()),
+		grpc.WithDefaultCallOptions(maxMsgRecvSize),
+		grpc.WithTransportCredentials(tlsCreds),
+		grpc.WithConnectParams(grpc.ConnectParams{
+			Backoff:           backoff.DefaultConfig,
+			MinConnectTimeout: defaultConnectTimeout,
+		}),
+	)
+
+	conn, err := grpc.Dial(daemonEndpoint, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to RPC server: %v", err)
+	}
+
+	return conn, nil
+}

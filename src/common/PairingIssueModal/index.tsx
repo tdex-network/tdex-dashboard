ddc06eed3792d7ee6fcc1c85b3e91bb949476fd2
Stéphane Roche <rochestephane@yahoo.fr>
2021-12-01 16:26:16 +0000

    Move PairingIssue modal (#98)

---
+++ src/common/PairingIssueModal/index.tsx
@@ -0,0 +1,32 @@
+import { Button, Modal } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface PairingIssueModalProps {
+  isPairingIssueModalVisible: boolean;
+}
+
+export const PairingIssueModal = ({ isPairingIssueModalVisible }: PairingIssueModalProps): JSX.Element => {
+  const navigate = useNavigate();
+  return (
+    <Modal
+      centered={true}
+      className="modal-error"
+      title="Pairing Issue"
+      visible={isPairingIssueModalVisible}
+      closable={false}
+      maskClosable={false}
+      maskStyle={{ backdropFilter: 'blur(6px)' }}
+      footer={
+        <Button key="goBack" onClick={() => navigate(-1)}>
+          Go back to pairing
+        </Button>
+      }
+    >
+      <p className="dm-mono dm-mono__x">
+        It seems that you haven't installed the TLS certificate properly or your Tdex daemon is not reachable.
+        Please go back and try again.
+      </p>
+    </Modal>
+  );
+};
--- src/features/walletUnlocker/OnboardingShowMnemonic.tsx
+++ src/features/walletUnlocker/OnboardingShowMnemonic.tsx
@@ -1,15 +1,12 @@
 import './onboardingShowMnemonic.less';
-import { Col, Row, Typography, Checkbox, Button, Modal, Spin } from 'antd';
+import { Col, Row, Typography, Checkbox, Spin } from 'antd';
 import type { CheckboxChangeEvent } from 'antd/es/checkbox';
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
 import { PasswordForm } from './PasswordForm';
 import { useGenSeedQuery } from './walletUnlocker.api';
 
 export const OnboardingShowMnemonic = (): JSX.Element => {
-  const navigate = useNavigate();
-  const [isGoBackModalVisible, setIsGoBackModalVisible] = useState<boolean>(false);
   const { data: seedMnemonicList, error: genSeedError } = useGenSeedQuery();
   const { Title } = Typography;
 
@@ -17,65 +14,38 @@ export const OnboardingShowMnemonic = (): JSX.Element => {
     console.log(`checked = ${e.target.checked}`);
   };
 
-  useEffect(() => {
-    if (genSeedError) {
-      setIsGoBackModalVisible(true);
-    }
-  }, [genSeedError]);
-
   return (
-    <>
-      <div id="show-mnemonic">
-        <Row justify="center">
-          <Col>
-            <Title level={2} className="dm-sans dm-sans__xx dm-sans__bold">
-              Secret Phrase
-            </Title>
-          </Col>
-        </Row>
-        <Row justify="center">
-          <Col>
-            <p className="dm-sans dm-sans__x">Save your 24-word recovery phrase, in the correct order</p>
-          </Col>
-        </Row>
-        <SeedMnemonicList seed={seedMnemonicList} />
-        <Row justify="center">
-          <Col>
-            <Checkbox onChange={onCheckboxChange} className="dm-sans dm-sans__x">
-              I’ve saved my Secret Phrase
-            </Checkbox>
-          </Col>
-        </Row>
-        <Row justify="center" className="mt-8">
-          <Col>
-            <Title level={3} className="dm-sans dm-sans__bold dm-sans__xx">
-              Set Password
-            </Title>
-          </Col>
-        </Row>
-        <PasswordForm mnemonic={seedMnemonicList || []} />
-        {genSeedError && <p className="error">{genSeedError}</p>}
-      </div>
-      <Modal
-        centered={true}
-        className="modal-error"
-        title="Pairing Issue"
-        visible={isGoBackModalVisible}
-        closable={false}
-        maskClosable={false}
-        maskStyle={{ backdropFilter: 'blur(6px)' }}
-        footer={
-          <Button key="goBack" onClick={() => navigate(-1)}>
-            Go back to pairing
-          </Button>
-        }
-      >
-        <p className="dm-mono dm-mono__x">
-          It seems that you haven't installed the TLS certificate properly or your Tdex daemon is not
-          reachable. Please go back and try again.
-        </p>
-      </Modal>
-    </>
+    <div id="show-mnemonic">
+      <Row justify="center">
+        <Col>
+          <Title level={2} className="dm-sans dm-sans__xx dm-sans__bold">
+            Secret Phrase
+          </Title>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <p className="dm-sans dm-sans__x">Save your 24-word recovery phrase, in the correct order</p>
+        </Col>
+      </Row>
+      <SeedMnemonicList seed={seedMnemonicList} />
+      <Row justify="center">
+        <Col>
+          <Checkbox onChange={onCheckboxChange} className="dm-sans dm-sans__x">
+            I’ve saved my Secret Phrase
+          </Checkbox>
+        </Col>
+      </Row>
+      <Row justify="center" className="mt-8">
+        <Col>
+          <Title level={3} className="dm-sans dm-sans__bold dm-sans__xx">
+            Set Password
+          </Title>
+        </Col>
+      </Row>
+      <PasswordForm mnemonic={seedMnemonicList || []} />
+      {genSeedError && <p className="error">{genSeedError}</p>}
+    </div>
   );
 };
 

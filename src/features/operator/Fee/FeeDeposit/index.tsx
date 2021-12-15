c6686e3d02e2b11363bb62016c837a8dc54a9f10
Janaka-Steph <rochestephane@yahoo.fr>
2021-12-15 12:27:52 +0000

    Refactor deposit page, opt-in fragmentation

--- src/features/operator/Fee/FeeDeposit/index.tsx
+++ src/features/operator/Fee/FeeDeposit/index.tsx
@@ -1,94 +1,90 @@
-import './feeDeposit.less';
-import Icon from '@ant-design/icons';
-import { Breadcrumb, Button, Col, notification, Row, Typography } from 'antd';
-import QRCode from 'qrcode.react';
+import { notification } from 'antd';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
-import alertOctogon from '../../../../assets/images/alert-octagon.svg';
-import { ReactComponent as chevronRight } from '../../../../assets/images/chevron-right.svg';
-import { HOME_ROUTE } from '../../../../routes/constants';
+import { useTypedSelector } from '../../../../app/store';
+import { DepositPage } from '../../../../common/DepositPage';
 import {
+  useClaimFeeDepositsMutation,
   useFeeFragmenterSplitFundsMutation,
+  useGetFeeAddressQuery,
   useGetFeeBalanceQuery,
   useGetFeeFragmenterAddressQuery,
 } from '../../operator.api';
 
-const { Text } = Typography;
-
 export const FeeDeposit = (): JSX.Element => {
+  const { explorerLiquidAPI } = useTypedSelector(({ settings }) => settings);
   const { refetch: refetchGetFeeBalance } = useGetFeeBalanceQuery();
-  const { data: feeFragmenterAddress, refetch: refetchGetFeeFragmenterAddress } =
+  const { data: feeFragmenterAddress, refetch: refetchFeeFragmenterAddress } =
     useGetFeeFragmenterAddressQuery({ numOfAddresses: 1 });
+  const { data: feeAddress, refetch: refetchFeeAddress } = useGetFeeAddressQuery();
   const [feeFragmenterSplitFunds] = useFeeFragmenterSplitFundsMutation();
+  const [claimFeeDeposits] = useClaimFeeDepositsMutation();
+
   const [isFragmenting, setIsFragmenting] = useState(false);
+  const [useFragmenter, setUseFragmenter] = useState(false);
+
+  const depositAddress = useFragmenter
+    ? feeFragmenterAddress?.[0].address || 'N/A'
+    : feeAddress?.[0].address || 'N/A';
 
   const handleFragmentFeeDeposits = async () => {
+    // @ts-ignore
+    const { data } = await feeFragmenterSplitFunds({ millisatsPerByte: 100, maxFragment: 50 });
+    data.on('status', async (status: any) => {
+      if (status.code === 0) {
+        await refetchGetFeeBalance();
+        notification.success({ message: 'Fragmentation deposit successful' });
+        setIsFragmenting(false);
+      } else {
+        console.error('status', status);
+        notification.error({ message: status.details });
+        setIsFragmenting(false);
+      }
+    });
+  };
+
+  const handleClaimFeeDeposits = async () => {
+    const response = await fetch(`${explorerLiquidAPI}/address/${depositAddress}/utxo`);
+    const utxoList = await response.json();
+    if (!utxoList.length) throw new Error('No utxo found. Did you wait for confirmation?');
+    const res = await claimFeeDeposits({
+      outpointsList: utxoList.map((u: any) => ({ hash: u.txid, index: u.vout })),
+    });
+    // @ts-ignore
+    if (res?.error) throw new Error(res?.error);
+    notification.success({ message: 'Deposit successful' });
+    setIsFragmenting(false);
+  };
+
+  const handleDeposit = async () => {
     try {
       setIsFragmenting(true);
-      // @ts-ignore
-      const { data } = await feeFragmenterSplitFunds({ millisatsPerByte: 100, maxFragment: 50 });
-      data.on('status', async (status: any) => {
-        if (status.code === 0) {
-          setIsFragmenting(false);
-          notification.success({ message: 'Fragmentation successful' });
-          await refetchGetFeeBalance();
-          await refetchGetFeeFragmenterAddress();
-        } else {
-          console.error('status', status);
-          notification.error({ message: status.details });
-          setIsFragmenting(false);
-        }
-      });
+      if (useFragmenter) {
+        await handleFragmentFeeDeposits();
+      } else {
+        await handleClaimFeeDeposits();
+      }
     } catch (err) {
+      setIsFragmenting(false);
       // @ts-ignore
-      notification.error({ message: err.message });
+      notification.error({ message: err.message, key: err.message });
     }
   };
 
   return (
-    <>
-      <Breadcrumb separator={<Icon component={chevronRight} />} className="mb-2">
-        <Breadcrumb.Item>
-          <Link to={HOME_ROUTE}>Dashboard</Link>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>Fee Deposit</Breadcrumb.Item>
-      </Breadcrumb>
-      <Row className="panel">
-        <Col span={12} className="pr-10">
-          <Button className="w-100 mb-4">CREATE NEW DEPOSIT ADDRESS</Button>
-        </Col>
-        <Col span={12}>
-          <Row className="panel panel__grey panel__top">
-            <Col span={8} offset={8}>
-              <QRCode className="qr-code" level="H" value={feeFragmenterAddress?.[0].address || ''} />
-            </Col>
-          </Row>
-          <Row className="py-6 fee-fragmenter-address">
-            <Col span={21} offset={1}>
-              <Text className="address" copyable>
-                {feeFragmenterAddress?.[0].address ?? 'N/A'}
-              </Text>
-            </Col>
-          </Row>
-          <Row className="panel panel__grey panel__bottom text-center">
-            <Col span={20} offset={2}>
-              <img src={alertOctogon} alt="alert" className="mb-2" />
-              <p>
-                The pending transaction will appear here, once you are done depositing assets click on CONFIRM
-                to claim your deposit.
-              </p>
-              <Row>
-                <Col span={20} offset={2}>
-                  <Button onClick={handleFragmentFeeDeposits} loading={isFragmenting}>
-                    CONFIRM DEPOSIT
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </>
+    <DepositPage
+      type="Fee"
+      depositAddress={depositAddress}
+      isFragmenting={isFragmenting}
+      handleDeposit={handleDeposit}
+      setUseFragmenter={setUseFragmenter}
+      getNewAddress={() => {
+        if (useFragmenter) {
+          refetchFeeFragmenterAddress();
+        } else {
+          refetchFeeAddress();
+        }
+      }}
+    />
   );
 };
--- src/features/operator/Market/MarketDeposit/index.tsx
+++ src/features/operator/Market/MarketDeposit/index.tsx
@@ -1,15 +1,10 @@
-import Icon from '@ant-design/icons';
-import { Breadcrumb, Button, Checkbox, Col, notification, Row, Typography } from 'antd';
-import type { CheckboxChangeEvent } from 'antd/es/checkbox';
-import QRCode from 'qrcode.react';
+import { notification } from 'antd';
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import type { MarketInfo } from '../../../../api-spec/generated/js/operator_pb';
 import { useTypedSelector } from '../../../../app/store';
-import alertOctogon from '../../../../assets/images/alert-octagon.svg';
-import { ReactComponent as chevronRight } from '../../../../assets/images/chevron-right.svg';
-import { HOME_ROUTE } from '../../../../routes/constants';
+import { DepositPage } from '../../../../common/DepositPage';
 import {
   useMarketFragmenterSplitFundsMutation,
   useGetMarketFragmenterAddressQuery,
@@ -18,76 +13,75 @@ import {
   useClaimMarketDepositsMutation,
 } from '../../operator.api';
 
-const { Text } = Typography;
-
 export const MarketDeposit = (): JSX.Element => {
   const { state } = useLocation() as { state: { marketInfo: MarketInfo.AsObject } };
   const { explorerLiquidAPI } = useTypedSelector(({ settings }) => settings);
-  const [useFragmenter, setUseFragmenter] = useState(false);
   const [marketFragmenterSplitFunds] = useMarketFragmenterSplitFundsMutation();
-  const [isFragmenting, setIsFragmenting] = useState(false);
-  const { data: marketFragmenterAddress, refetch: refetchGetMarketFragmenterAddress } =
+  const { data: marketFragmenterAddress, refetch: refetchMarketFragmenterAddress } =
     useGetMarketFragmenterAddressQuery({ numOfAddresses: 1 });
-  //
-  const { data: marketAddress } = useGetMarketAddressQuery({
+  const { data: marketAddress, refetch: refetchMarketAddress } = useGetMarketAddressQuery({
     baseAsset: state?.marketInfo.market?.baseAsset || '',
     quoteAsset: state?.marketInfo.market?.quoteAsset || '',
   });
   const [claimMarketDeposits] = useClaimMarketDepositsMutation();
-  //
   const { refetch: refetchDeposits } = useListDepositsQuery({
     accountIndex: state?.marketInfo.accountIndex,
   });
-  const address = useFragmenter
+
+  const [useFragmenter, setUseFragmenter] = useState(false);
+  const [isFragmenting, setIsFragmenting] = useState(false);
+
+  const depositAddress = useFragmenter
     ? marketFragmenterAddress?.[0].address || 'N/A'
     : marketAddress?.[0].address || 'N/A';
 
   const handleFragmentMarketDeposits = async () => {
-    try {
-      setIsFragmenting(true);
-      // @ts-ignore
-      const { data } = await marketFragmenterSplitFunds({
-        market: {
-          baseAsset: state?.marketInfo?.market?.baseAsset || '',
-          quoteAsset: state?.marketInfo?.market?.quoteAsset || '',
-        },
-        millisatsPerByte: 100,
-      });
-      data.on('status', async (status: any) => {
-        if (status.code === 0) {
-          setIsFragmenting(false);
-          notification.success({ message: 'Fragmentation successful' });
-          await refetchGetMarketFragmenterAddress();
-          await refetchDeposits();
-        } else {
-          console.error('status', status);
-          notification.error({ message: status.details });
-          setIsFragmenting(false);
-        }
-      });
-    } catch (err) {
-      // @ts-ignore
-      notification.error({ message: err.message });
-    }
+    // @ts-ignore
+    const { data } = await marketFragmenterSplitFunds({
+      market: {
+        baseAsset: state?.marketInfo?.market?.baseAsset || '',
+        quoteAsset: state?.marketInfo?.market?.quoteAsset || '',
+      },
+      millisatsPerByte: 100,
+    });
+    data.on('status', async (status: any) => {
+      if (status.code === 0) {
+        await refetchDeposits();
+        notification.success({ message: 'Fragmentation deposit successful' });
+        setIsFragmenting(false);
+      } else {
+        console.error('status', status);
+        notification.error({ message: status.details, key: status.details });
+        setIsFragmenting(false);
+      }
+    });
   };
 
-  const handleMarketDeposits = async () => {
+  const handleClaimMarketDeposits = async () => {
+    const response = await fetch(`${explorerLiquidAPI}/address/${depositAddress}/utxo`);
+    const utxoList = await response.json();
+    if (!utxoList.length) throw new Error('No utxo found. Did you wait for confirmation?');
+    const res = await claimMarketDeposits({
+      outpointsList: utxoList.map((u: any) => ({ hash: u.txid, index: u.vout })),
+      market: {
+        baseAsset: state?.marketInfo?.market?.baseAsset || '',
+        quoteAsset: state?.marketInfo?.market?.quoteAsset || '',
+      },
+    });
+    // @ts-ignore
+    if (res?.error) throw new Error(res?.error);
+    notification.success({ message: 'Deposit successful' });
+    setIsFragmenting(false);
+  };
+
+  const handleDeposit = async () => {
     try {
       setIsFragmenting(true);
-      const response = await fetch(`${explorerLiquidAPI}/address/${address}/utxo`);
-      const utxoList = await response.json();
-      if (!utxoList.length) throw new Error('No utxo found. Did you wait for confirmation?');
-      const res = await claimMarketDeposits({
-        outpointsList: utxoList.map((u: any) => ({ hash: u.txid, index: u.vout })),
-        market: {
-          baseAsset: state?.marketInfo?.market?.baseAsset || '',
-          quoteAsset: state?.marketInfo?.market?.quoteAsset || '',
-        },
-      });
-      // @ts-ignore
-      if (res?.error) throw new Error(res?.error);
-      notification.success({ message: 'Deposit successful' });
-      setIsFragmenting(false);
+      if (useFragmenter) {
+        await handleFragmentMarketDeposits();
+      } else {
+        await handleClaimMarketDeposits();
+      }
     } catch (err) {
       setIsFragmenting(false);
       // @ts-ignore
@@ -95,59 +89,20 @@ export const MarketDeposit = (): JSX.Element => {
     }
   };
 
-  const handleCheckboxChange = (ev: CheckboxChangeEvent) => {
-    setUseFragmenter(ev.target.checked);
-  };
-
   return (
-    <>
-      <Breadcrumb separator={<Icon component={chevronRight} />} className="mb-2">
-        <Breadcrumb.Item>
-          <Link to={HOME_ROUTE}>Dashboard</Link>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>Market Deposit</Breadcrumb.Item>
-      </Breadcrumb>
-      <Row className="panel">
-        <Col span={12} className="pr-10">
-          <Button className="w-100 mb-4">CREATE NEW DEPOSIT ADDRESS</Button>
-          <Checkbox onChange={handleCheckboxChange} className="dm-sans dm-sans__x mt-4">
-            Advanced: split deposit into multiple fragments for concurrent trade support
-          </Checkbox>
-        </Col>
-        <Col span={12}>
-          <Row className="panel panel__grey">
-            <Col span={8} offset={8}>
-              <QRCode className="qr-code" level="H" value={address} />
-            </Col>
-          </Row>
-          <Row className="my-6">
-            <Col span={21} offset={1}>
-              <Text className="address" copyable>
-                {address}
-              </Text>
-            </Col>
-          </Row>
-          <Row className="panel panel__grey text-center">
-            <Col span={20} offset={2}>
-              <img src={alertOctogon} alt="alert" className="mb-2" />
-              <p>
-                The pending transaction will appear here, once you are done depositing assets click on CONFIRM
-                to claim your deposit.
-              </p>
-              <Row>
-                <Col span={20} offset={2}>
-                  <Button
-                    onClick={useFragmenter ? handleFragmentMarketDeposits : handleMarketDeposits}
-                    loading={isFragmenting}
-                  >
-                    CONFIRM DEPOSIT
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </>
+    <DepositPage
+      type="Market"
+      depositAddress={depositAddress}
+      isFragmenting={isFragmenting}
+      handleDeposit={handleDeposit}
+      setUseFragmenter={setUseFragmenter}
+      getNewAddress={() => {
+        if (useFragmenter) {
+          refetchMarketFragmenterAddress();
+        } else {
+          refetchMarketAddress();
+        }
+      }}
+    />
   );
 };

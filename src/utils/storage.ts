e5e5ef5542639c1d4166f60e9f753b4fb514b30e
Janaka-Steph <rochestephane@yahoo.fr>
2022-03-01 11:09:37 +0000

    Update Tauri to v1.0.0

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -10,32 +10,31 @@ function getFileNameForKey(key: string): string {
   return `tdex-dashboard-${key.replace('persist:', '')}`;
 }
 
-// getFilePath uses getFileNameForKey in order to return the full path of a key's file
-function getFilePath(key: string): string {
-  return `tdex-dashboard/${getFileNameForKey(key)}`;
-}
-
 // tauriStorage will be used to persist reducers in filesystem
 export const tauriStorage: Storage = {
   async getItem(key: string) {
-    const json = await tauriAPIs.fs?.readTextFile(getFilePath(key), { dir: tauriAPIs.fs?.Dir.App });
-    return JSON.parse(json || '');
+    try {
+      const json = await tauriAPIs.fs?.readTextFile(getFileNameForKey(key), { dir: tauriAPIs.fs?.Dir.App });
+      return JSON.parse(json || '');
+    } catch (err) {
+      console.error('readTextFile', err);
+    }
   },
   async setItem(key: string, value: any) {
     if ('__TAURI__' in window) {
       try {
-        await tauriAPIs.fs?.readDir('tdex-dashboard', { dir: tauriAPIs.fs?.Dir.App });
+        await tauriAPIs.fs?.readDir('', { dir: tauriAPIs.fs?.Dir.App });
       } catch (err) {
-        await tauriAPIs.fs?.createDir('tdex-dashboard', { dir: tauriAPIs.fs?.Dir.App, recursive: true });
+        await tauriAPIs.fs?.createDir('', { dir: tauriAPIs.fs?.Dir.App, recursive: true });
       }
       await tauriAPIs.fs?.writeFile(
-        { path: getFilePath(key), contents: JSON.stringify(value) },
+        { path: getFileNameForKey(key), contents: JSON.stringify(value) },
         { dir: tauriAPIs.fs?.Dir.App }
       );
     }
   },
   async removeItem(key: string) {
-    await tauriAPIs.fs?.removeFile(getFilePath(key), { dir: tauriAPIs.fs?.Dir.App });
+    await tauriAPIs.fs?.removeFile(getFileNameForKey(key), { dir: tauriAPIs.fs?.Dir.App });
   },
 };
 

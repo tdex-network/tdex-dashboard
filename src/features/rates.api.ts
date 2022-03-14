f459edc28214cf3b60517c37fdefc19ed5a3bb6e
librehodl <45250003+librehodl@users.noreply.github.com>
2022-03-13 23:54:20 -0700

    Add coingecko rtkQuery hook

--- src/features/rates.api.ts
+++ src/features/rates.api.ts
@@ -1,15 +1,29 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { CURRENCIES } from './../utils/constants';
+import type { CurrencyInterface } from './settings/settingsSlice';
+
+export type CoinGeckoPriceResult = Record<string, Record<CurrencyInterface['value'], number>>;
+export const LBTC_COINGECKOID = 'bitcoin';
+export const USDT_COINGECKOID = 'tether';
 
 export const ratesApi = createApi({
   reducerPath: 'ratesApi',
-  baseQuery: fakeBaseQuery<string>(),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.coingecko.com/api/v3',
+  }),
   endpoints: (build) => ({
-    getPriceFromCoinGecko: builder.query<any, any>({
-      queryFn: async (currencies) => {
-        console.log('ratesApi')
+    latestPriceFromCoinGecko: build.query<CoinGeckoPriceResult, void>({
+      query: () => {
+        const ids = [LBTC_COINGECKOID, USDT_COINGECKOID].join(',');
+        const vs_currencies = CURRENCIES.map((currency) => currency.value).join(',');
+        return {
+          url: `/simple/price`,
+          params: { ids, vs_currencies },
+        };
       },
     }),
   }),
-})
+});
 
-export const { getPriceFromCoinGecko } = ratesApi
+export const { useLatestPriceFromCoinGeckoQuery } = ratesApi;

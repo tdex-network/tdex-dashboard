1caeba59b88dca9508351313db3236967df565a1
Stéphane Roche <rochestephane@yahoo.fr>
2022-04-29 13:56:04 +0100

    Display daemon version (#335)

---
+++ src/features/settings/DaemonVersion.tsx
@@ -0,0 +1,22 @@
+import { Col, Row, Typography } from 'antd';
+
+import { useGetInfoQuery } from '../operator/operator.api';
+
+const { Title } = Typography;
+
+export const DaemonVersion = (): JSX.Element => {
+  const { data: daemonInfo } = useGetInfoQuery();
+
+  return (
+    <div className="mb-4 panel panel__grey">
+      <Row>
+        <Col span={24}>
+          <Title className="dm-sans dm-sans__x dm-sans__bold dm-sans__grey" level={3}>
+            Daemon Version
+          </Title>
+        </Col>
+      </Row>
+      <div>{daemonInfo?.buildData?.version || 'N/A'}</div>
+    </div>
+  );
+};

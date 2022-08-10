9ec38cbf804a085058661c6f3d6507b43d3dbb48
Stéphane Roche <rochestephane@yahoo.fr>
2022-08-10 16:16:05 +0200

    Fix updateMarketPrice and price display (#387)

--- src/features/operator/Market/MarketSettings/UpdateMarketPriceForm.tsx
+++ src/features/operator/Market/MarketSettings/UpdateMarketPriceForm.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import type { Market } from '../../../../api-spec/protobuf/gen/js/tdex/v1/types_pb';
 import type { RootState } from '../../../../app/store';
 import { useTypedDispatch, useTypedSelector } from '../../../../app/store';
-import { formatFiatToSats, formatLbtcUnitToSats, isLbtcAssetId } from '../../../../utils';
+import { fromUnitToUnit, isLbtcAssetId } from '../../../../utils';
 import { operatorApi } from '../../operator.api';
 
 interface IFormInputs {
@@ -43,12 +43,13 @@ export const UpdateMarketPriceForm = ({
           operatorApi.endpoints.updateMarketPrice.initiate({
             market,
             price: {
+              // If asset is Bitcoin set price in L-BTC unit
               basePrice: isLbtcAssetId(market?.baseAsset, network)
-                ? Number(formatLbtcUnitToSats(values.basePrice, lbtcUnit))
-                : Number(formatFiatToSats(values.basePrice)),
+                ? Number(fromUnitToUnit(values.basePrice, lbtcUnit, 'L-BTC'))
+                : values.basePrice,
               quotePrice: isLbtcAssetId(market?.quoteAsset, network)
-                ? Number(formatLbtcUnitToSats(values.quotePrice, lbtcUnit))
-                : Number(formatFiatToSats(values.quotePrice)),
+                ? Number(fromUnitToUnit(values.quotePrice, lbtcUnit, 'L-BTC'))
+                : values.quotePrice,
             },
           })
         ).unwrap();

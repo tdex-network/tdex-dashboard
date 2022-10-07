9441f298781d8d52feebd409a9987c8d56e58363
Janaka-Steph <rochestephane@yahoo.fr>
2022-10-07 15:22:54 +0200

    Fix baseUrl in packaged mode

--- src/features/walletUnlocker/OnboardingPairing.tsx
+++ src/features/walletUnlocker/OnboardingPairing.tsx
@@ -130,8 +130,11 @@ export const OnboardingPairing = (): JSX.Element => {
         ('__TAURI__' in window || ('USE_PROXY' in window && Boolean((window as any).USE_PROXY))) &&
         !!connectData?.cert;
       dispatch(setUseProxy(isProxyNeeded));
-      if (!isProxyNeeded) {
-        dispatch(setBaseUrl(`${connectData?.proto}://` + connectData?.host));
+      if (!isProxyNeeded && !isPackaged) {
+        dispatch(setBaseUrl(`${connectData?.proto}://${connectData?.host}`));
+      }
+      if (!isProxyNeeded && isPackaged) {
+        dispatch(setBaseUrl(`${connectData?.proto}://${window.location.host}`));
       }
       dispatch(setTdexdConnectUrl(tdexdConnectUrl));
       dispatch(setConnectUrlProto(connectData?.proto));

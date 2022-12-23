2b1b998c3e230c9ba4eab95b14ad467f317cbbf5
Janaka-Steph <rochestephane@yahoo.fr>
2022-12-23 18:37:37 +0100

    Collected Swap Fees tooltip

--- src/features/operator/Market/MarketOverview/CollectedSwapFees.tsx
+++ src/features/operator/Market/MarketOverview/CollectedSwapFees.tsx
@@ -1,5 +1,5 @@
 import { InfoCircleOutlined } from '@ant-design/icons';
-import { Col, Row, Typography } from 'antd';
+import { Col, Popover, Row, Typography } from 'antd';
 import React from 'react';
 
 import type { MarketReport } from '../../../../api-spec/protobuf/gen/js/tdex-daemon/v1/types_pb';
@@ -24,7 +24,15 @@ export const CollectedSwapFees = ({ marketReport, state }: CollectedSwapFeesProp
           <Title className="dm-sans dm-sans__x dm-sans__bold dm-sans__grey d-inline mr-4" level={3}>
             Collected Swap Fees
           </Title>
-          <InfoCircleOutlined className="grey" />
+          <Popover
+            trigger="hover"
+            content='Collected swap fees refer to the fees you have collected from traders. The fee is
+          charged in the asset "on the way in", which means that if the swap is from USDT to BTC, the fee is
+          charged in USDT. The fee can be configured as a percentage of the overall value of the swap, or as a
+          fixed amount in the asset on the way in.'
+          >
+            <InfoCircleOutlined className="grey" />
+          </Popover>
         </Col>
       </Row>
       <Row align="middle">

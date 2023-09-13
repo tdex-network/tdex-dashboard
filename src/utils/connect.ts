e2a4bdfe7e19690194453100719f9275dd465e5b
Janaka-Steph <kakobaba1212@protonmail.com>
2023-09-13 17:51:50 +0200

    Check if 'tdex.v2.WalletService' when calling Reflection

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -157,7 +157,7 @@ export const getProtoVersion = async (providerEndpoint: string, proto: string): 
     const data = await res.json();
     const isVersion2 = data.result.listServicesResponse.service
       .map((s: any) => s.name)
-      .includes('tdex.v2.TransportService');
+      .includes('tdex.v2.WalletService');
     return isVersion2 ? 'v2' : 'v1';
   } catch (err) {
     return 'v1';
